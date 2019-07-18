f07b864a3f00dabdad3dd080ae50b738aa9b840d
yq_mc <573380218@qq.com>
2019-07-17 22:05:03 -0400

    add

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const os = require("os");
 const fs = require("fs");
 const EventEmitter = require("events")
+const http = require("http");
+const _ = require("underscore");
 
 const files = fs.readdirSync("./"); // return all the files' names in the folder
 console.log(files);
@@ -21,14 +23,34 @@ function p()
 //logger.log("122"); // call a module
 
 var totalmem = os.totalmem();
+console.log("-------------------------------");
+
 //Template string
 console.log(`Total Memory: ${totalmem}`); // print the total memory
+console.log("-------------------------------");
 
 // Listen an event with event argument and arrow function
 const my_eventemitter = new logger();
 my_eventemitter.on("message_logged", (arg)=>{
     console.log("received message by the on()!", arg);
+}); // listen for the event
+my_eventemitter.log("my message");
+console.log("-------------------------------");
+
+// http module
+const server = http.createServer((req, res)=>{
+    if(req.url == "/")
+    {
+        res.write("Hello World!");
+        res.end();
+    }
+    if(req.url == "/courses")
+    {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
 });
+server.listen(port = 3000); // listen for port 3000
 
-my_eventemitter.log("my message")
-
+console.log("Listening at port 3000...");
+console.log("-------------------------------");
