48d7a2d02c3529176b2dd91e9634597a53c16a22
yq_mc <573380218@qq.com>
2019-07-26 15:23:11 -0400

    add

--- express_demo/app.js
+++ express_demo/app.js
@@ -1,49 +1,11 @@
+const courses = require("./route/courses");
 const express = require("express");
 const app = express();
 
 app.use(express.json()); // middle layer
-
-// add new middle layer
-//app.use(function(req, res, next){
-//   console.log("Logging....");
-//    next();
-//})
-
-// emulate database
-courses = [{id: 1, name:"course1"},
-{id: 2, name:"course2"},
-{id: 3, name:"course3"}];
-
-// express
-// GET
-app.get('/api/courses', (req, res)=>{
-    res.send(courses);
-    console.log("GET new course....");
-    return ;
-});
-
-// POST
-app.post('/api/courses', (req, res)=>{
-    const course = [{id:courses.length+1, name: req.body.name}];
-    courses.push(course);
-    console.log(courses);
-    return ;
-});
-
-// PUT
-app.put('/api/courses/:id', (req, res)=>{
-    const course = courses.find(req.params.id);
-    course.name = req.body.name;
-    return ;
-});
-
-// DELETE
-//app.delete('api/courses/:id', (req, res)=>{
-//
-//})
+app.use("/api/courses", courses);
 
 const port = process.env.PORT || 3000;
 app.listen(port, (req, res)=>{
     console.log('Listening at port ',port);
 });
-
---
+++ express_demo/route/courses.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+// emulate database
+courses = [{id: 1, name:"course1"},
+{id: 2, name:"course2"},
+{id: 3, name:"course3"}];
+
+// GET
+router.get('/', (req, res)=>{
+    res.send(courses);
+    console.log("GET new course....");
+    return ;
+});
+
+// POST
+router.post('/', (req, res)=>{
+    const course = [{id:courses.length+1, name: req.body.name}];
+    courses.push(course);
+    return courses;
+});
+
+// PUT
+router.put('/:id', (req, res)=>{
+    const course = courses.find(req.params.id);
+    course.name = req.body.name;
+    return ;
+});
+
+// DELETE
+//app.delete('api/courses/:id', (req, res)=>{
+//
+//})
+
+module.exports = router;
